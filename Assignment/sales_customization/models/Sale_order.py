ad05ec3cd582e706e5de659cde0cb17fe7aeb493
lakhichand borse <labo@odoo.com>
2023-12-18 15:00:23 +0530

    update

--- Assignment/sales_customization/models/Sale_order.py
+++ Assignment/sales_customization/models/Sale_order.py
@@ -9,3 +9,14 @@ class SaleOrderLineCustom(models.Model):
         self.set_estimated_shipping_date(values)
 
         return super(SaleOrderLineCustom, self).create(values)
+
+    def set_estimated_shipping_date(self, values):
+        
+        carrier_id = values.get('order_id') and self.env['sale.order'].browse(values['order_id']).carrier_id.id or self.carrier_id.id
+        if carrier_id:
+            customisation = self.env['delivery.customisation'].search([('carrier_id', '=', carrier_id)], limit=1)
+            if customisation:
+                max_shipping_days = customisation.max_shipping_days
+                estimated_shipping_date = fields.Datetime.now() + timedelta(days=max_shipping_days)
+                values['estimated_shipping_date'] = estimated_shipping_date
+    
